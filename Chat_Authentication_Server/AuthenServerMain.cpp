7087ca156408d9968412305c377c55a7f9249eda
Ana Djurkovic <ana_djurkovic@hotmail.com>
2021-11-08 12:35:38 -0500

    Connected to SQL server

--- Chat_Authentication_Server/AuthenServerMain.cpp
+++ Chat_Authentication_Server/AuthenServerMain.cpp
@@ -1,8 +1,29 @@
 #include <gen/authentication.pb.h>
+#include <mysql/jdbc.h>
 
+using namespace sql::mysql;
+
+void ConnectToMySQLServer()
+{
+	try 
+	{
+		// Returns a mySQL driver pointer
+		MySQL_Driver* driver = sql::mysql::get_driver_instance();
+	}
+	catch (sql::SQLException e)
+	{
+		printf("Failed to get driver instance with error %s\n", e.what());
+		return;
+	}
+	printf("Successfully retrieved the driver instance\n");
+
+}
 
 int main(int argc, char** argv)
 {
+	// To create a mySQL connection, we need a driver
+	ConnectToMySQLServer();
+	system("Pause");
 
 	return 0;
 }
